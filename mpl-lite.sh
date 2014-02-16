af39eb2991ae00aaf90f3bca1116126142ba938b
Stuart Mumford <stuart@mumford.me.uk>
2014-02-16 22:09:42 +0000

    actually download the mpl source

--- mpl-lite.sh
+++ mpl-lite.sh
@@ -1,4 +1,4 @@
-#wget https://downloads.sourceforge.net/project/matplotlib/matplotlib/matplotlib-1.3.1/matplotlib-1.3.1.tar.gz
+wget https://downloads.sourceforge.net/project/matplotlib/matplotlib/matplotlib-1.3.1/matplotlib-1.3.1.tar.gz
 tar -xvf matplotlib-1.3.1.tar.gz
 cd  matplotlib-1.3.1
 rm -r lib/matplotlib/tests/baseline_images/*
